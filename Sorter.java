70404de53b7a7fe37bb3c8714e680e226b138485
Abdullah <abdullah@Abdullahs-Mac.local>
2023-02-13 14:56:39 +0300

    Add insertion sort to Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -1,22 +1,33 @@
-public class Sorter{
+public class Sorter {
 
     void selectionSort(int arr[]) {
         int n = arr.length;
 
-        for (int i = 0; i < n-1; i++)
-        {
-           
+        for (int i = 0; i < n - 1; i++) {
+
             int min_idx = i;
-            for (int j = i+1; j < n; j++){
+            for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
             }
-        
+
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
-            
+
+        }
+    }
+
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
         }
     }
 }
-
